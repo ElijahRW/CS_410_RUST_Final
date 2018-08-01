6f9483608eef81b0199891ab67e42ec66e728282
Elijah Rich-Wimmer <elijah.rich-wimmer@act-on.net>
2018-07-31 20:22:43 -0700

    Foundational Commit -Copy of Piston example Code and assets with intention of initial compilation and revision. (Borrowed from: https://github.com/PistonDevelopers/piston-examples) -Added Initial Cargo Configuration -Minor Revision to ReadMe file.

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# CS_410_RUST_Final
+# CS 410 RUST Final
 An XML parser to generate a game GUI:
 
 
-# Overview:
-In short, this repository is an attempt to develop a library capabile of sepperating a game GUI from the actual game display. In many modern game engines this is a standard approach. 
+## Overview:
+In short, this repository is an attempt to develop a library capable of separating a game GUI from 
+the actual game display. In many modern game engines this is a standard approach. 
 
-XML files contain button implementation details and settings. Each time a program is reloaded (or a command manually triggered), the XML file is reparsed and the buttons, healthbars, menus are reinstantiated (Allowing for dynamic configuration).
+XML files will contain button implementation details and settings. Each time a program is reloaded 
+(or a command manually triggered), the XML file is reparsed and the buttons, healthbars, menus are 
+reinstantiated (Allowing for dynamic configuration).
+
+Inspiration/Reference Using concepts from Cryengine 5: http://docs.cryengine.com/display/CEPROG/UI+Element
+
+
+##XML Syntax
+Currently, XML files must adhere to a standardized syntax to be properly read into the parser currently. 
+However
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+/*
+***Copied Code from Piston examples
+***Current purpose is to provide an example window to demonstrate UI load
+***comment will be revised once this source file has been adequately
+***changed to reflect original content
+***For original source, see:
+*/
+extern crate piston_window;
+extern crate find_folder;
+
+use piston_window::*;
+
+
+//EPRW this file simply runs
+fn main() {
+    let mut window: PistonWindow = WindowSettings::new(
+        "piston: hello_world",
+        [200, 200]
+    )
+        .exit_on_esc(true)
+        //.opengl(OpenGL::V2_1) // Set a different OpenGl version
+        .build()
+        .unwrap();
+
+    let assets = find_folder::Search::ParentsThenKids(3, 3)
+        .for_folder("assets").unwrap();
+    println!("{:?}", assets);
+    let ref font = assets.join("FiraSans-Regular.ttf");
+    let factory = window.factory.clone();
+    let mut glyphs = Glyphs::new(font, factory, TextureSettings::new()).unwrap();
+
+    window.set_lazy(true);
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g| {
+            let transform = c.transform.trans(10.0, 100.0);
+
+            clear([0.0, 0.0, 0.0, 1.0], g);
+            text::Text::new_color([0.0, 1.0, 0.0, 1.0], 32).draw(
+                "Hello world!",
+                &mut glyphs,
+                &c.draw_state,
+                transform, g
+            ).unwrap();
+        });
+    }
+}
+
+/* END OF COPIED SOURCES */
